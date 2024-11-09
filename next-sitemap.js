c2fec25858f6d0e8e0ecd1a12559216e18280994
Amartya Chowdhury <work.amar05@yahoo.com>
2024-11-09 23:47:15 +0530

    fix next-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,23 +1,29 @@
-
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policy: [
+    policies: [
       {
-        userAgent: '*',
-        disallow: ['/api', '/_next', '/404'],
+        userAgent: "*",
+        disallow: ["/api", "/_next", "/404"],
       },
     ],
   },
-  changefreq: 'monthly',
+  changefreq: "monthly",
   priority: 0.8,
   transform: async (config, path) => {
-    if (path === '/') {
+    if (path === "/") {
       return {
         loc: path,
+        changefreq: "monthly",
         priority: 1.0,
       };
     }
+
+    return {
+      loc: path,
+      changefreq: "monthly",
+      priority: 0.8,
+    };
   },
 };
