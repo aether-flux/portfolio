ea0c4c804010da3d65a08e16af30eec917fc7a69
Amartya Chowdhury <work.amar05@yahoo.com>
2024-11-10 11:29:10 +0530

    fix site preview in links

--- app/layout.js
+++ app/layout.js
@@ -37,8 +37,8 @@ export const metadata = {
     images: [
       {
         url: `${siteUrl}/og-img.jpg`,
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 630,
         alt: "My Portfolio Image",
       },
     ],
